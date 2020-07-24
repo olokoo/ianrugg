9de7101a1847256d3135803b93239d37a11db99b
Ian Rugg <ian@olokoo.com>
2020-07-23 19:30:43 -0600

    More testing and added full res images to repo

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,7 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
   const blogPost = path.resolve(`./src/templates/blog-post.js`)
+  const projectPage = path.resolve(`./src/templates/project-page.js`)
   const result = await graphql(
     `
       {
@@ -47,7 +48,22 @@ exports.createPages = async ({ graphql, actions }) => {
         next,
       },
     })
-  })
+  });
+
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    const next = index === 0 ? null : posts[index - 1].node
+
+    createPage({
+      path: post.node.fields.slug,
+      component: projectPage,
+      context: {
+        slug: post.node.fields.slug,
+        previous,
+        next,
+      },
+    })
+  });
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
