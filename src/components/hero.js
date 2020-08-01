e33d269523b538141109717d3de807553f5ec576
Ian Rugg <ian@olokoo.com>
2020-07-31 19:53:25 -0600

    Added dynamic text logic to the home page hero.

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,14 +1,5 @@
-/**
- * Bio component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
-// import Image from "gatsby-image"
-import { Link } from "gatsby"
-
+import homePage from '../content/settings/home-page.yml'
 
 const Hero = () => {
   return (
@@ -20,12 +11,13 @@ const Hero = () => {
           </div>
         </div>
         <div class="hero-content">
-          <p>My name is Ian Rugg and I like to build stuff. All different sorts of stuff. But professionally, I build websites with my friends at <a href="https://redvanworkshop.com" target="_new">Red Van Workshop!</a></p>
+          <p>
+            {homePage.heroText}
+          </p>
         </div>
       </div>
     </section>
   )
 }
-
 
 export default Hero
